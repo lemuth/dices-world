f0c20a36be471035356158ef9995054e89704e2b
lemuth <gilles.saley@gmail.com>
2025-02-10 12:35:23 +0100

    configure github action

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
+const isProd = process.env.NODE_ENV === 'production';
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
+    distDir: 'dist',
+    basePath: isProd ? '/dices-world' : '',
+
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
+
