2d7f27a2dbb7809714d0491d5c31d0444e0b4efd
lemuth <gilles.saley@gmail.com>
2025-02-11 16:40:42 +0100

    ajout font

--- src/lib/fonts.jsx
+++ src/lib/fonts.jsx
@@ -1,10 +1,15 @@
-import { Henny_Penny } from "next/font/google"
+import { Henny_Penny, Chewy } from "next/font/google"
 
 const hennyPenny = Henny_Penny({
     subsets: ['latin'],
     weight: '400'
 })
+const chewy = Chewy({
+    subsets: ['latin'],
+    weight: '400'
+})
 
 export {
-    hennyPenny
+    hennyPenny,
+    chewy
 }
